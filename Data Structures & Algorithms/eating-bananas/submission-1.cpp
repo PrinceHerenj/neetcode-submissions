c961b9e4284c55123975e57701a3f2ff53cbe7c7
Sneh Prince Herenj <54269513+PrinceHerenj@users.noreply.github.com>
2026-06-29 21:21:12 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1, r = *max_element(piles.begin(), piles.end());
+        int res = r;
+        while (l <= r) {
+            int k = l + (r - l) / 2;
+            if (sumByK(piles, k) <= h) {
+                res = k;
+                r = k - 1;
+            }
+            else l = k + 1;
+        }
+
+        return res;
+    }
+
+    long long sumByK(vector<int>& piles, int k) {
+        long long res = 0;
+        for (auto p: piles) {
+            res += ceil((double)p / k);
+        }
+        return res;
+    }
+};
