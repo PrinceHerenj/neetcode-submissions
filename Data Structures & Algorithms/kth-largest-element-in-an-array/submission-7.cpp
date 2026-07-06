940dbd36de08f0e1b9973f3914dd217db3d7c758
Sneh Prince Herenj <54269513+PrinceHerenj@users.noreply.github.com>
2026-07-06 12:59:25 +0530

    Add: kth-largest-element-in-an-array - submission-7

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-7.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        k = nums.size() - k;
+        return quickSelect(nums, 0, nums.size() - 1, k);
+    }
+
+    int quickSelect(vector<int>& nums, int l, int r, int k) {
+        int pivot = nums[r];
+        int p = l;
+
+        for (int i = l; i < r; i++) {
+            if (nums[i] < pivot) {
+                swap(nums[i], nums[p]);
+                p++;
+            }
+        }
+        swap(nums[p], nums[r]);
+
+        if (k > p) {
+            return quickSelect(nums, p + 1, r, k);
+        } else if (k < p) {
+            return quickSelect(nums, l, p - 1, k);
+        } else return nums[p];
+    }
+};
