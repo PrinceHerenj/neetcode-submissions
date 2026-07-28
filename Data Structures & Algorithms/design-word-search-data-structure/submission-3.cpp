07e59064429cad9720e63f45a67e4e4137f67a52
Sneh Prince Herenj <54269513+PrinceHerenj@users.noreply.github.com>
2026-07-28 23:12:20 +0530

    Add: design-word-search-data-structure - submission-3

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-3.cpp
@@ -0,0 +1,40 @@
+class WordDictionary {
+    class TrieNode {
+    public:
+        unordered_map<char, TrieNode*> children;
+        bool word;
+    };
+public:
+    TrieNode* root;
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    void addWord(string word) {
+        TrieNode* curr = root;
+        for (auto ch: word) {
+            if (!curr->children.contains(ch)) curr->children[ch] = new TrieNode();
+            curr = curr->children[ch];
+        }
+        curr->word = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* curr = root;
+        return searchInNode(word, 0, curr);
+    }
+
+    bool searchInNode(string word, int index, TrieNode* node) {
+        if (index == word.size()) return node->word;
+        if (word[index] == '.') {
+            for (auto& [key, childNode]: node->children) {
+                if (searchInNode(word, index + 1, childNode)) return true;
+            }
+            return false;
+        } else {
+            char ch = word[index];
+            if (!node->children.contains(ch)) return false;
+            return searchInNode(word, index + 1, node->children[ch]);
+        }
+    }
+};
