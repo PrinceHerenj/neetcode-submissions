1e535e019463163bd8166a445ee23c09670fbe0b
Sneh Prince Herenj <54269513+PrinceHerenj@users.noreply.github.com>
2026-08-07 00:30:36 +0530

    Add: ipo - submission-0

---
+++ Data Structures & Algorithms/ipo/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
+        priority_queue<int> maxProfit;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minCapital;
+
+        for (int i = 0; i < profits.size(); i++) {
+            minCapital.push({capital[i], profits[i]});
+        }
+
+        for (int i = 0; i < k; i++) {
+            while (!minCapital.empty() && minCapital.top().first <= w) {
+                maxProfit.push(minCapital.top().second);
+                minCapital.pop();
+            }
+            if (maxProfit.empty()) break;
+            w += maxProfit.top();
+            maxProfit.pop();
+        }
+        
+        return w;
+    }
+};
