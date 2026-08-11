44fd4e7cfcd2b56fc1025480e4ac15378b458f2f
Sneh Prince Herenj <54269513+PrinceHerenj@users.noreply.github.com>
2026-08-11 23:43:58 +0530

    Add: kruskal - submission-1

---
+++ Data Structures & Algorithms/kruskal/submission-1.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    class UnionFind {
+    public:
+        unordered_map<int, int> parent;
+        unordered_map<int, int> rank;
+
+        UnionFind(int n) {
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+            }
+        }
+
+        int find(int x) {
+            int p = parent[x];
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+
+            return p;
+        }
+
+        bool merge(int x, int y) {
+            int p1 = find(x), p2 = find(y);
+            if (p1 == p2) return false;
+
+            if (rank[p1] < rank[p2]) {
+                parent[p1] = p2;
+            } else if (rank[p2] < rank[p1]) {
+                parent[p2] = p1;
+            } else {
+                parent[p2] = p1;
+                rank[p1]++;
+            }
+
+            return true;
+        }
+    };
+
+
+    int minimumSpanningTree(vector<vector<int>>& edges, int n) {
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<> > minHeap;
+
+        for (auto& edge: edges) {
+            minHeap.push({edge[2], edge[0], edge[1]});
+        }
+
+        UnionFind uf(n);
+        int visitedCount = 0;
+        int totalWeight = 0;
+
+        while (visitedCount < n - 1 && !minHeap.empty()) {
+            auto [wt, n1, n2] = minHeap.top();
+            minHeap.pop();
+
+            if (!uf.merge(n1, n2)) continue;
+
+            visitedCount++;
+            totalWeight += wt;
+        }
+
+        return (visitedCount == n - 1) ? totalWeight : -1;
+    }
+};
