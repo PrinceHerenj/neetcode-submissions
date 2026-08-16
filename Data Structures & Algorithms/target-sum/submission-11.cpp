1152379b8adcd9d59f166ae0642731ffa5768b8e
Sneh Prince Herenj <54269513+PrinceHerenj@users.noreply.github.com>
2026-08-16 15:35:12 +0530

    Add: target-sum - submission-11

---
+++ Data Structures & Algorithms/target-sum/submission-11.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    unordered_map<string, int> dp;
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return dfs(0, 0, nums, target);
+    }
+
+    int dfs(int i, int curSum, vector<int>& nums, int target) {
+        if (i == nums.size())
+            return curSum == target;
+
+        string key = to_string(i) + ',' + to_string(curSum);
+        if (dp.contains(key)) return dp[key];
+            return dp[key] = dfs(i + 1, curSum + nums[i], nums, target) + dfs(i + 1, curSum - nums[i], nums, target);
+    }
+};
