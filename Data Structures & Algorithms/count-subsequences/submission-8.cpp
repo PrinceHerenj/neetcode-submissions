767a4a677c7a78555658ef9f5ce47725e1a9ac01
Sneh Prince Herenj <54269513+PrinceHerenj@users.noreply.github.com>
2026-08-20 00:36:00 +0530

    Add: count-subsequences - submission-8

---
+++ Data Structures & Algorithms/count-subsequences/submission-8.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int numDistinct(string s, string t) {
+        dp.resize(s.size(), vector<int>(t.size(), -1));
+        return dfs(0, 0, s, t);
+    }
+
+    int dfs(int i, int j, string& s, string& t) {
+        if (j == t.size()) return 1;
+        if (i == s.size()) return 0;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        dp[i][j] = dfs(i + 1, j, s, t);
+
+        if (s[i] == t[j]) {
+            dp[i][j] += dfs(i + 1, j + 1, s, t);
+        }
+
+        return dp[i][j];
+    }
+};
