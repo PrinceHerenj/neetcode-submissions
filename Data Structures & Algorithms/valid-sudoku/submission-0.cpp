335a178c2a9b950dc413ff106d410dadbb6030b2
Sneh Prince Herenj <54269513+PrinceHerenj@users.noreply.github.com>
2026-08-26 15:45:33 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<int, unordered_set<int>> rows;
+        unordered_map<int, unordered_set<int>> cols;
+        unordered_map<int, unordered_set<int>> squares;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    if (rows[i].contains(board[i][j])
+                    or cols[j].contains(board[i][j])
+                    or squares[(i / 3) * 3 + (j / 3)].contains(board[i][j])) 
+                        return false;
+                    rows[i].insert(board[i][j]);
+                    cols[j].insert(board[i][j]);
+                    squares[(i / 3) * 3 + (j / 3)].insert(board[i][j]);
+                }
+            }
+        }
+
+        return true;
+        
+    }
+};
