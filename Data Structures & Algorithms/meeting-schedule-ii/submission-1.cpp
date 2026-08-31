82bb07f5b60347cb72781afc23ef47173ed698b9
Sneh Prince Herenj <54269513+PrinceHerenj@users.noreply.github.com>
2026-08-31 23:33:53 +0530

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector<int> starts, ends;
+        for (auto [start, end]: intervals) {
+            starts.push_back(start);
+            ends.push_back(end);
+        }
+
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+
+        int i = 0, j = 0;
+        int res = 0;
+        int count = 0;
+        while (i < intervals.size()) {
+            if (starts[i] < ends[j]) {
+                i++;
+                count++;
+            } else {
+                j++;
+                count--;
+            }
+            res = max(res, count);
+        }
+
+        return res;
+    }
+};
