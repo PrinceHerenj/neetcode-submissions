faf33057ba7bcfc7b61faecc8f19083a60ef41f9
Sneh Prince Herenj <54269513+PrinceHerenj@users.noreply.github.com>
2026-09-03 00:16:14 +0530

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0, j = 0;
+        string res;
+        while (i < word1.size() and j < word2.size()) {
+            if (i <= j) res.push_back(word1[i++]);
+            else res.push_back(word2[j++]);
+        }
+
+        while (i < word1.size()) res.push_back(word1[i++]);
+        while (j < word2.size()) res.push_back(word2[j++]);
+
+        return res;
+    }
+};
